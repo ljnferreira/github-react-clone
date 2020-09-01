3ccffa8704a801685bae9c3f588c77bab55c244f
Lucas Ferreira <ljnferreira@gmail.com>
2020-08-31 21:45:06 -0300

    feat: added  global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles'
 import Routes from './routes';
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyles/>
       <Routes/>
-    </div>
+    </>
   );
 }
 
