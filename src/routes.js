c039e744b4c9b6c5eb071eba4f60fe2cae332fbb
Lucas Ferreira <ljnferreira@gmail.com>
2020-08-31 21:40:45 -0300

    feat: created navbar component

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,19 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Repository from './pages/Repository';
 
+import NavBar from './components/NavBar'
+
 function Routes() {
   return (
     <Router>
+      <NavBar/>
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/users/:user' component={Profile}/>
