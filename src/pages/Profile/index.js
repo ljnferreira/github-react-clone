bf329b3615f2d3da2d55681071cfe5e9dce66346
Lucas Ferreira <ljnferreira@gmail.com>
2020-08-31 21:46:31 -0300

    refactor: added state to userData

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { Container } from './styles';
 
-function Profile(props) {
+import api from '../../services/api';
+
+function Profile({match}, props) {
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    async function load(){
+      const user = decodeURIComponent(match.params.user);
+
+      const userData = await api.get(`/users/${user}`);
+
+      setUserData(userData.data);
+
+    }
+
+    load();
+
+  },[match.params.user]);
+
   return (
-    <h1>Profile</h1>
+    <Container>
+      <img src={userData?.avatar_url} alt={userData?.name}/>
+      <h1>{userData?.name}</h1>
+      <h2>{userData?.login}</h2>
+    </Container>
+
   );
 }
 
